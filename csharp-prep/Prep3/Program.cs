49d098130ece3d5fad88d83623c13cf2068bd1e0
JaysonUhi <jaysonuhi@gmail.com>
2024-06-15 18:49:22 -0600

    prep 3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("What is the magic number?: ");
+        // int magic = int.Parse(Console.ReadLine());
+        // Console.WriteLine("What is your guess?: ");
+        // int guess = int.Parse(Console.ReadLine());
+
+        Random randomNumber = new Random();
+        int magic = randomNumber.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magic)
+        {
+            Console.Write("What is your guess?: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magic > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
